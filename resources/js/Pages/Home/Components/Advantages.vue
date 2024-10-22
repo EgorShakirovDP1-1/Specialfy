c7a7ba05c117c58a14705446a2fb45956909655c
ssoraaaaa <sigats99@gmail.com>
2024-10-22 13:35:25 +0300

    upd Advantages.vue

--- resources/js/Pages/Home/Components/Advantages.vue
+++ resources/js/Pages/Home/Components/Advantages.vue
@@ -2,15 +2,16 @@
     <section class="p-lg-5 mt-4" aria-labelledby="postSharingBenefitsHeading">
         <div class="container text-center">
             <h1 id="postSharingBenefitsHeading" class="mb-1 text-dark">
-                Supermen are not who save the world, but those who put their effort into work and fun!
+                Sometimes you come across things you can't do by yourself...
+                <br><br>
+                We got people that can do it for you!
             </h1>
 
-            <div class="row justify-content-center">
+            <div class="row justify-content-center mt-6">
                 <div class="col-lg-6" s>
                     <div class="card bg-light text-dark" role="region" aria-label="All Inclusive">
                         <div class="card-body text-center text-dark">
                             <div class="h1 mb-3">
-                                <i class="bi bi-bag-check text-dark"></i>
                             </div>
                             <h3 class="card-title mb-3">All Inclusive</h3>
                             <p class="card-text text-dark lead">
