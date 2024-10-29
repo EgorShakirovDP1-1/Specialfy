d57f13f5938a27a914321991be30b1825defff84
31DP1EShak <101180000+EgorShakirovDP1-1@users.noreply.github.com>
2024-10-29 15:40:16 +0200

    Add files via upload

---
+++ database/2024_02_21_120612_create_likes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade') -> Unique();
+            $table->foreignId('post_id')->constrained()->onDelete('cascade') -> Unique();
+            $table->boolean('value')->default(0);
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('likes');
+    }
+};
