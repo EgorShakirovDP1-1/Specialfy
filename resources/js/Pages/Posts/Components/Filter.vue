6497451a5330cd2b77a613483635a14386be32c2
EgorShakirovDP1-1 <11DEShakirov@rvt.lv>
2024-11-04 14:46:15 +0200

    image handling logic working blyat

--- resources/js/Pages/Posts/Components/Filter.vue
+++ resources/js/Pages/Posts/Components/Filter.vue
@@ -3,35 +3,19 @@
 
      
         <div class="filter-box rounded p-4 mx-3 mb-2 background">
-            <h4 class="text-center">Categories</h4>
-            <div class="one-line">
-               
-            </div>
-            <div class="one-line">
-                
-            </div>
-        </div>
+    <h4 class="text-center">Categories</h4>
+    <select @change="handleSetCategoryFilter($event.target.value)" class="form-select">
+        <option value="">All Categories</option>
+        <option v-for="category in categories" :key="category.id" :value="category.id">
+            {{ category.name }}
+        </option>
+    </select>
+</div>
 
-      
-       
-
-   
-    
-       
-        
-
-      
-      
-
-   
-        
-    
 </template>
 
 <script setup>
 
-const emit = defineEmits([
-   ])
 
 
 
